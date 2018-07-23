409a377657d49877e9727ea4adf5ceb54b30ffeb
Yuichi Saito <munisystem@gmail.com>
2018-07-23 10:22:12 +0900

    Implement background script

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,10 @@
-console.log("hello")
+chrome.webRequest.onBeforeRequest.addListener(function(t) {
+    const [scheme, hostpath] = t.url.split("://")
+    const shortcut = "go/"
+
+    if (hostpath.slice(0, shortcut.length) === shortcut) {
+        return {redirectUrl: "http://localhost:80" + hostpath.slice(shortcut.length - 1)}
+    }
+}, {
+    urls: ["all_urls>"]
+}, ["blocking"]);
